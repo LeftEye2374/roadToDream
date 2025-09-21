d1f4bac4fe4d75dc478f625615a6cd9b91d8c0e8
Borissov M.R. <shinnn2374@mail.ru>
2025-09-22 01:05:35 +0300

    ebal win

---
+++ ConsoleCSharpConsoleApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleCSharpConsoleApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("****************************************************");
+            Console.WriteLine("Welcome to my console app");
+            Console.WriteLine("****************************************************");
+            Console.ReadLine();
+        }
+    }
+}

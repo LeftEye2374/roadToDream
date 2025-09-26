08d72081b978db86e64460c78932e61a11fed8d5
Borissov M.R. <shinnn2374@mail.ru>
2025-09-26 14:32:40 +0300

    add cmd file

---
+++ ChapterThree_AllProject/SimpleCSharpApp/SimpleCSharpApp.cmd
@@ -0,0 +1,18 @@
+@echo off
+ rem Пакетный файл для приложения SimpleCSharpApp.exe,
+ rem в котором захватывается возвращаемое им значение.
+ dotnet run
+ @if "%ERRORLEVEL%" == "0" goto success
+ :fail
+ rem Приложение потерпело неудачу,
+ echo This application has failed!
+ echo return value = %ERRORLEVEL%
+ goto end
+ :success
+ rem Приложение завершилось успешно,
+ echo This application has succeeded!
+ echo return value = %ERRORLEVEL%
+ goto end
+ :end
+ rem Все готово,
+ echo All Done.

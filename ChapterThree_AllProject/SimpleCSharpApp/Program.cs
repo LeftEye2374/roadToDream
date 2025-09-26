900ced9ee703264e73dc3a36538ad13f53334ecf
Borissov M.R. <shinnn2374@mail.ru>
2025-09-26 14:37:49 +0300

    all

--- ChapterThree_AllProject/SimpleCSharpApp/Program.cs
+++ ChapterThree_AllProject/SimpleCSharpApp/Program.cs
@@ -3,4 +3,11 @@
 Console.WriteLine("My first c# app");
 Console.WriteLine("Hello world");
 Console.WriteLine();
+
+for (int i = 0; i < args.Length; i++)
+{
+    Console.WriteLine("Args: {0}", args {i} );
+}
+
 Console.ReadLine();
+return 0;

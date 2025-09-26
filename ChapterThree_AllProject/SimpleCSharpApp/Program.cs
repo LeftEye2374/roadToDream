1b1e1408ff1a3f2df03fc02a6a254f3d5e0211cf
Borissov M.R. <shinnn2374@mail.ru>
2025-09-26 14:23:07 +0300

    123

--- ChapterThree_AllProject/SimpleCSharpApp/Program.cs
+++ ChapterThree_AllProject/SimpleCSharpApp/Program.cs
@@ -1,13 +1,6 @@
-﻿namespace SimpleCSharpApp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("My first c# app");
-            Console.WriteLine("Hello world");
-            Console.WriteLine("");
-            Console.ReadLine();
-        }
-    }
-}
+﻿using System;
+
+Console.WriteLine("My first c# app");
+Console.WriteLine("Hello world");
+Console.WriteLine();
+Console.ReadLine();

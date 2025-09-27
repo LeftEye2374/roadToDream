8561d77f4e75e4ce3548199b9f22ac21f10f8c70
Borissov M.R. <shinnn2374@mail.ru>
2025-09-27 13:13:10 +0300

    message

--- ChapterThree_AllProject/SimpleCSharpApp/Program.cs
+++ ChapterThree_AllProject/SimpleCSharpApp/Program.cs
@@ -4,9 +4,9 @@ Console.WriteLine("My first c# app");
 Console.WriteLine("Hello world");
 Console.WriteLine();
 
-for (int i = 0; i < args.Length; i++)
+foreach(String arg in args)
 {
-    Console.WriteLine("Args: {0}", args {i} );
+    Console.WriteLine("Arg: {0}", arg);
 }
 
 Console.ReadLine();

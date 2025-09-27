e377d385a1cc8331c7232ecd7823944eecfed603
Borissov M.R. <shinnn2374@mail.ru>
2025-09-28 01:35:15 +0300

    new datas dec

--- ChapterThree_AllProject/BasicDataTypes/Program.cs
+++ ChapterThree_AllProject/BasicDataTypes/Program.cs
@@ -17,3 +17,14 @@ static void DefaultDeclarations()
     Console.WriteLine("=> Default Declarations");
     int myInt = default;
 }
+
+static void NewingDataType()
+{
+    Console.WriteLine("=> Using new to create variables:");
+    bool b = new bool();
+    int i = new int();
+    double d = new double();
+    DateTime dt = new DateTime();
+    Console.WriteLine("{ 0},{ 1},{ 2},{ 3}", b, i, d, dt);
+    Console.WriteLine();
+}

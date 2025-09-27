465c8b6f39b1a3337d36ab4f74180482da7037f3
Borissov M.R. <shinnn2374@mail.ru>
2025-09-28 01:59:54 +0300

    char datas

--- ChapterThree_AllProject/BasicDataTypes/Program.cs
+++ ChapterThree_AllProject/BasicDataTypes/Program.cs
@@ -53,3 +53,18 @@ static void DataTypeFunctionality()
     double.Negativelnfinity);
     Console.WriteLine();
 }
+
+static void CharFunctionality()
+{
+    Console.WriteLine("=> char type Functionality:");
+    char myChar = 'a';
+    Console.WriteLine("char.IsDigit('a'): {0}"f
+   char.IsDigit(myChar));
+    Console.WriteLine("char.IsLetter('a'): {0}", char.IsLetter(myChar));
+    Console.WriteLine("char.IsWhiteSpace('Hello There', 5): {0}",
+    char.IsWhiteSpace("Hello There", 5));
+    Console.WriteLine("char.IsWhiteSpace('Hello There', 6): {0}",
+    char.IsWhiteSpace("Hello There", 6));
+    Console.WriteLine("char.IsPunctuation('?'): {0}",
+    char.IsPunctuation('?'));
+    Co

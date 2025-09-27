2b07e1a60579afd0eb3e249954cc1da2f144e2e1
Borissov M.R. <shinnn2374@mail.ru>
2025-09-27 22:12:25 +0300

    BasicDataTypes

--- ChapterThree_AllProject/BasicDataTypes/Program.cs
+++ ChapterThree_AllProject/BasicDataTypes/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Numerics;
+Console.WriteLine("Fun with Basic Data Types \n");
+
+
+static void LocalVarDeclarations()
+{
+    Console.WriteLine("=> Data Declarations:");
+    int mylnt = 0;
+    string myString;
+    myString = "This is my character data";
+    Console.WriteLine();
+}

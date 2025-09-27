58dc27d508752360debba29aa98c4149daaaceef
Borissov M.R. <shinnn2374@mail.ru>
2025-09-28 02:01:21 +0300

    char parse

--- ChapterThree_AllProject/BasicDataTypes/Program.cs
+++ ChapterThree_AllProject/BasicDataTypes/Program.cs
@@ -67,4 +67,19 @@ static void CharFunctionality()
     char.IsWhiteSpace("Hello There", 6));
     Console.WriteLine("char.IsPunctuation('?'): {0}",
     char.IsPunctuation('?'));
-    Co
+    Console.WriteLine(); 
+}
+
+static void ParseFromStrings()
+{
+    Console.WriteLine("=> Data type parsing:");
+    bool b = bool.Parse("True");
+    Console.WriteLine("Value of b: {0}", b);
+    double d = double.Parse("99.884");
+    Console.WriteLine("Value of d: {0}", d);
+    int i = int.Parse("8");
+    Console.WriteLine("Value of i: {0}", i);
+    char c = Char.Parse("w");
+    Console.WriteLine("Value of c: {0}", c);
+    Console.WriteLine();
+}

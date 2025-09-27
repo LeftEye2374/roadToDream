165ec459d9dce1b076619d2ed32ac14ad7fe9f46
Borissov M.R. <shinnn2374@mail.ru>
2025-09-27 21:48:26 +0300

    IO two

--- ChapterThree_AllProject/BasicConsoleIO/Program.cs
+++ ChapterThree_AllProject/BasicConsoleIO/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.WriteLine("Basic console IO");
+GetUserData();
+Console.ReadLine();
+
+static void GetUserData()
+{
+    Console.WriteLine("Please write your name: ");
+    string name = Console.ReadLine();
+    Console.WriteLine("Please write your age: ");
+    string age = Console.ReadLine();
+
+    ConsoleColor prevColor = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Yellow;
+
+    Console.WriteLine("Hello {0}! You are {1} years old.", name, age);
+    Console.ForegroundColor = prevColor;
+}

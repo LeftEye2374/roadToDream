d7253ce414593209756dd825e8214774b1d6df81
Borissov M.R. <shinnn2374@mail.ru>
2025-10-01 04:33:28 +0300

    conv

--- ChapterThree_AllProject/TransferData/Program.cs
+++ ChapterThree_AllProject/TransferData/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System
+
+Console.WriteLine("Fun with type convers");
+short num1 = 9;
+short num2 = 10;
+Console.WriteLine("{0} + {1} = {2}", num1, num2, Add(num1, num2));
+Console.ReadLine();
+
+static int Add(short num1, short num2)
+{
+    return num1 + num2;
+}

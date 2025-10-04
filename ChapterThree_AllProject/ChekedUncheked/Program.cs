8cd103a1588e43aeaac6ad2fc51eadea2773a629
Borissov M.R. <shinnn2374@mail.ru>
2025-10-04 14:36:13 +0300

    Chek/unchek

--- ChapterThree_AllProject/ChekedUncheked/Program.cs
+++ ChapterThree_AllProject/ChekedUncheked/Program.cs
@@ -2,10 +2,10 @@
 {
     static void Main(String[] args)
     {
-        ProcessByte();
+        ProcessByteCheked();
     }
 
-    static void ProcessByte()
+    static void ProcessByteCheked()
     {
         byte b1 = 100;
         byte b2 = 250;
@@ -18,6 +18,26 @@
         {
             Console.WriteLine(e.Message);
         }
+    }
+
+    static void ProcessByte()
+    {
+        byte b1 = 100;
+        byte b2 = 250;
+        try
+        {
+            byte sum = unchecked((byte)Add(b1, b2));
+            Console.WriteLine("Sum = {0}", sum);
+        }
+        catch (OverflowException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    static int Add(int b1, int b2)
+    {
+        return b1 + b2;
     }
 
     static int Add(int b1, int b2)

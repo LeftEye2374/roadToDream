4de05976d459f566c0360ab0a6550ecf332408c7
Borissov M.R. <shinnn2374@mail.ru>
2025-10-04 13:35:38 +0300

    Chek/unchek

--- ChapterThree_AllProject/ChekedUncheked/Program.cs
+++ ChapterThree_AllProject/ChekedUncheked/Program.cs
@@ -2,5 +2,19 @@
 {
     static void Main(String[] args)
     {
+
+    }
+
+    static void ProcessByte()
+    {
+        byte b1 = 100;
+        byte b2 = 250;
+        byte sum = byte(Add(b1, b2));
+        Console.WriteLine("Sum = {0}", sum);
+    }
+
+    static int Add(int b1, int b2)
+    {
+        return b1 + b2; 
     }
 }

10feab623c6a8dc4a175216cea204c70b7d35007
Borissov M.R. <shinnn2374@mail.ru>
2025-10-05 16:22:06 +0300

    foreach with linq

--- ChapterThree_AllProject/IterationOp/Program.cs
+++ ChapterThree_AllProject/IterationOp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 class Program
 {
@@ -6,6 +7,7 @@ class Program
     {
         ForLoopExample();
         ForEachLoopExample();
+        LinkqQueryOverInts();
     }
 
     static void ForLoopExample()
@@ -21,6 +23,17 @@ class Program
         string[] carTypes = {"BMW", "Mercedeas", "Audi", "Honda" };
         foreach (string carType in carTypes) {
             Console.WriteLine(carType);
+        }
+    }
+
+    static void LinkqQueryOverInts()
+    {
+        int[] numbers = {10, 20, 30, 40 , 1, 4, 5, 8 };
+        var subset = from i in numbers where i < 10 select i;
+        Console.WriteLine("Values in subset: ");
+        foreach (var i in subset)
+        {
+            Console.WriteLine("{0}", i);
         }
     }
 }

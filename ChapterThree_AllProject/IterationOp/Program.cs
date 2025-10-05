d94e6399de9d1a50cb458d537c2acdb7ea5a2bde
Borissov M.R. <shinnn2374@mail.ru>
2025-10-05 16:03:33 +0300

    for loop example

--- ChapterThree_AllProject/IterationOp/Program.cs
+++ ChapterThree_AllProject/IterationOp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        ForLoopExample();
+
+    }
+
+    static void ForLoopExample()
+    {
+        for(int i =0; i < 5; i++)
+        {
+            Console.WriteLine("Aslan huisos");
+        }
+    }
+}

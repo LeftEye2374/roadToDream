ff7663ed5c6e248989a2842a273f5986343683d3
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-10-06 13:13:55 +0300

    arrays

--- ChapterFour_AllProject/Arrays/Program.cs
+++ ChapterFour_AllProject/Arrays/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main()
     {
-        
+        Console.WriteLine("Fun with Arrayes");
+        SimpleArray();
+        Console.ReadLine();
+    }
+
+    static void SimpleArray()
+    {
+        Console.WriteLine("=> Simple Array creation");
+        int[] myInts = new int[3];
+        string[] booksOnDotNet =  new string[100];
+        Console.WriteLine("");
     }
 }

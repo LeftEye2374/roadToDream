ea5e2a3cdadb32999c44f9944085c0100382dcb0
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-10-06 13:18:11 +0300

    ...

--- ChapterFour_AllProject/Arrays/Program.cs
+++ ChapterFour_AllProject/Arrays/Program.cs
@@ -5,15 +5,35 @@ class Program
     static void Main()
     {
         Console.WriteLine("Fun with Arrayes");
+        Console.WriteLine("___________________________");
         SimpleArray();
-        Console.ReadLine();
+        Console.WriteLine("___________________________");
+        ArrayInit();
     }
 
     static void SimpleArray()
     {
-        Console.WriteLine("=> Simple Array creation");
-        int[] myInts = new int[3];
-        string[] booksOnDotNet =  new string[100];
-        Console.WriteLine("");
+        int[] myInts = new int[5];
+        myInts[0] = 1;
+        myInts[1] = 2;
+        myInts[2] = 3;
+        myInts[3] = 4;
+        myInts[4] = 5;
+
+        foreach (int i in myInts)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    static void ArrayInit()
+    {
+        string[] stringArray = new string[3] { "one", "two", "three" };
+        Console.WriteLine("String array has length - {0}", stringArray.Length);
+        int[] myInts =  new int[3] {3, 15, 45};
+        foreach (int i in myInts)
+        {
+            Console.WriteLine(i);
+        }
     }
 }

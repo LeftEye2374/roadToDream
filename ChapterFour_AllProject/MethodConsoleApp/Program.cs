767f6519468ffb43f923057b6fc8133172396cb1
Borissov M.R. <shinnn2374@mail.ru>
2025-10-07 17:26:48 +0300

    include method

--- ChapterFour_AllProject/MethodConsoleApp/Program.cs
+++ ChapterFour_AllProject/MethodConsoleApp/Program.cs
@@ -3,8 +3,19 @@
     static void Main(string[] args)
     {
         Console.WriteLine(Add(1, 2));
+        Console.WriteLine("_____________________________________________");
+        Console.WriteLine(AddWrapper(1, 2));
         Console.ReadLine();
     }
 
     static int Add(int x, int y) => x + y;
+
+    static int AddWrapper(int x, int y)
+    {
+        return Add(x, y);
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
 }

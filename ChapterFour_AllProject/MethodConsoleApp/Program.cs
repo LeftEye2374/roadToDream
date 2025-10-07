b69dd30bd2519b828dbee906aaf3a3e2af616918
Borissov M.R. <shinnn2374@mail.ru>
2025-10-07 17:16:00 +0300

    method

---
+++ ChapterFour_AllProject/MethodConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Add(1, 2));
+        Console.ReadLine();
+    }
+
+    static int Add(int x, int y) => x + y;
+}

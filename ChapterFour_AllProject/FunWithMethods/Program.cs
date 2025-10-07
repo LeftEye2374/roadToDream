8d38c9055905d8238f66fc8d5de403e3a2080509
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-10-07 21:01:40 +0300

    ...

---
+++ ChapterFour_AllProject/FunWithMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Add(1, 2));
+        Console.ReadLine();
+    }
+    
+    static int Add(int x, int y)
+    {
+        int ans =  x + y;
+        x = 10000;
+        y = 5000;
+        return ans;
+    }
+}

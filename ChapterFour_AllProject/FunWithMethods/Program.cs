6827050fdec589d0cda2950d2d59f010a2210877
Borissov M.R. <shinnn2374@mail.ru>
2025-10-09 16:42:44 +0300

    param params finished

--- ChapterFour_AllProject/FunWithMethods/Program.cs
+++ ChapterFour_AllProject/FunWithMethods/Program.cs
@@ -5,10 +5,12 @@
         Console.WriteLine(Add(1, 2));
         Console.ReadLine();
         Console.WriteLine("___________________________________");
+
         int ans;
         AddUsingOutParam(90, 90, out ans);
         Console.ReadLine();
         Console.WriteLine("___________________________________");
+
         string s1 = "Flip";
         string s2 = "Flop";
         Console.WriteLine("Before {0} , {1}", s1, s2);
@@ -16,10 +18,19 @@
         Console.WriteLine("After {0}, {1}", s1, s2);
         Console.ReadLine();
         Console.WriteLine("___________________________________");
+
         Console.WriteLine(AddOnlyRead(123123, 123123123));
         Console.ReadLine();
         Console.WriteLine("___________________________________");
 
+        double average;
+        average = CalculateAverage(4.0, 3.2, 5.7, 64.22, 87.2);
+        Console.WriteLine("Average of data is: {0}", average);
+        double[] data = {4.0, 3.2, 5.7};
+        average = CalculateAverage(data);
+        Console.WriteLine("Average of data is: {0}", average);
+        Console.WriteLine("Average of data is: {0}", CalculateAverage());
+        Console.ReadLine();
     }
 
     static int Add(int x, int y)

68038c664479c2f1bad63ff04c92112cee840dd4
Borissov M.R. <shinnn2374@mail.ru>
2025-10-09 18:20:18 +0300

    enums

---
+++ ChapterFour_AllProject/FunWithEnum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+Console.WriteLine("*********** Fun with enums ***********");
+EmpTypeEnum emp = EmpTypeEnum.Contractor;
+AskForBonus(emp);
+Console.ReadLine();
+
+
+enum EmpTypeEnum : byte
+{
+    Manager = 102,
+    Grunt, // =103
+    Contractor, // =104
+    VicePresident // =105
+}
+
+static void AskForBonus(EmpTypeEnum e)
+{
+    switch (e)
+    {
+        case EmpTypeEnum.Manager:
+            Console.WriteLine("How about stock options instead?");
+            break;
+        case EmpTypeEnum.Grunt:
+            Console.WriteLine("You have got to be kidding...");
+            break;
+        case EmpTypeEnum.Contractor:
+            Console.WriteLine("You already get enough cash...");
+            break;
+        case EmpTypeEnum.VicePresident:
+            Console.WriteLine("VERY GOOD, Sir");
+            break;
+    }
+
+}

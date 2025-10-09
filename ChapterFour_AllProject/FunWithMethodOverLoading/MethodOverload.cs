3b1cd424eafe7e47904d25948fe9798d34f80b8d
Borissov M.R. <shinnn2374@mail.ru>
2025-10-09 18:04:49 +0300

    overload method

---
+++ ChapterFour_AllProject/FunWithMethodOverLoading/MethodOverload.cs
@@ -0,0 +1,21 @@
+namespace MethodOverload
+{
+
+    public static class AddOperations
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static long Add(long x, long y)
+        {
+            return x + y;
+        }
+    }
+}
--- ChapterFour_AllProject/FunWithMethodOverLoading/Program.cs
+++ ChapterFour_AllProject/FunWithMethodOverLoading/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using MethodOverload;
+using static MethodOverload.AddOperations;
+
+Console.WriteLine("*********** Fun with Method Overloading ***********");
+Console.WriteLine(Add(10,10));
+Console.WriteLine(Add(900_000_000_000, 900_000_000_000));
+Console.WriteLine(Add(4.3, 4.5));
+Console.ReadLine();
+
+

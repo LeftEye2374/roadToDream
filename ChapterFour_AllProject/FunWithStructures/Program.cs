99e9663f5524ba3eeb0a91ad3a39dc6aaf651432
Borissov M.R. <shinnn2374@mail.ru>
2025-10-10 16:21:45 +0300

    structures

---
+++ ChapterFour_AllProject/FunWithStructures/Program.cs
@@ -0,0 +1,20 @@
+﻿struct Point
+{
+    public int X;
+    public int Y;
+
+    public void increment()
+    {
+        X++; Y++;
+    }
+
+    public void decrement()
+    {
+        X--; Y--;
+    }
+
+    public void display()
+    {
+        Console.WriteLine("X = {0}, Y = {1}", X,Y);
+    }
+}

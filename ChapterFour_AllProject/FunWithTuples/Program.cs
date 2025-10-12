e47c0ba08961c283ae58bf4e1fe8bd3721d3abe6
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-10-12 17:45:11 +0300

    chapter four is end

--- ChapterFour_AllProject/FunWithTuples/Program.cs
+++ ChapterFour_AllProject/FunWithTuples/Program.cs
@@ -3,8 +3,14 @@
     static void Main(string[] args)
     {
         Console.WriteLine("Fun with Tuples");
-        var foo = new (Prop1 = "first", Prop2 = "second");
-        var bar = (foo.Prop1, foo.Prop2);
-        Console.WriteLine($"{bar.Prop1}, {bar.Prop2}");
+        var sample = FeelTheseValues();
+        Console.WriteLine(sample.a);
+        Console.WriteLine(sample.b);
+        Console.WriteLine(sample.c);
     }
+
+    static (int a, string b, bool c) FeelTheseValues()
+    {
+        return(16,"hui", true);
+    } 
 }

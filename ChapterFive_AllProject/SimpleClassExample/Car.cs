2b325b848ed5f301a7fcb9190839fa363d6f2a5f
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-10-12 17:57:54 +0300

    OOP in c#

---
+++ ChapterFive_AllProject/SimpleClassExample/Car.cs
@@ -0,0 +1,19 @@
+namespace SimpleClassExample;
+
+public class Car
+{
+    public string petName;
+    public int currSpeed;
+
+    public void SpeedUp(int delta)
+    {
+        currSpeed += delta;
+    }
+
+    public void PrintState()
+    {
+        Console.WriteLine("{0} is going {1} MPH." , petName, currSpeed);
+    }
+
+}
+ 
---
+++ ChapterFive_AllProject/SimpleClassExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SimpleClassExample;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car myCar = new Car();
+        myCar.petName = "hui";
+        myCar.currSpeed = 200;
+        
+        myCar.PrintState();
+        Console.WriteLine("____________________");
+        myCar.SpeedUp(40);
+        myCar.PrintState();
+    }
+}

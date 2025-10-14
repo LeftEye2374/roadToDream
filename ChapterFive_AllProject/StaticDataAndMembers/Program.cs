89f87c79466cc88236f3b6e82afbb002da8e6074
Borissov M.R. <shinnn2374@mail.ru>
2025-10-14 18:07:50 +0300

    static data

--- ChapterFive_AllProject/StaticDataAndMembers/Program.cs
+++ ChapterFive_AllProject/StaticDataAndMembers/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+using StaticDataAndMembers;
+
+Console.WriteLine("Fun with statuic data");
+SavingsAccount s1 = new SavingsAccount(50);
+SavingsAccount s2 = new SavingsAccount(1000);
+SavingsAccount s3 = new SavingsAccount(900.4);
+Console.ReadLine();

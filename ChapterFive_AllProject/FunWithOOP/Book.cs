0abd6e3fad1c8aadd1b71fd148b13fc065ad8ee8
Borissov M.R. <shinnn2374@mail.ru>
2025-10-15 16:14:08 +0300

    OOP start

---
+++ ChapterFive_AllProject/FunWithOOP/Book.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Book
+{
+    public int numberOfPages;
+
+    public Book() { }
+
+    public Book(int numberOfPages)
+    {
+        this.numberOfPages = numberOfPages;
+    }
+}

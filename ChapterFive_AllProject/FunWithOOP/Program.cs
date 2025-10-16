f946743da7e1ed1528553e6f9958f4db97511e90
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-10-16 17:14:37 +0300

    ;...

--- ChapterFive_AllProject/FunWithOOP/Program.cs
+++ ChapterFive_AllProject/FunWithOOP/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Fun with OOP");
+        Book book = new Book();
+        Console.WriteLine(book);
+        Console.WriteLine("_____________");
+        
+    }
+}

9e593f1bab4c0d1263b276ba66db2b01db6ab8ad
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-10-18 02:08:50 +0300

    ...

---
+++ ChapterFive_AllProject/.Incapsul/Car.cs
@@ -0,0 +1,12 @@
+namespace Incapsul;
+
+public class Car
+{
+    private string carName ="";
+
+    public string PetName
+    {
+        get { return carName; }
+        set { carName = value; }
+    }
+}

c85448bb42a2787e03b2ff31f6307e8ad0f6589f
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-10-18 02:15:00 +0300

    ...

--- ChapterFive_AllProject/.AutoProps/Program.cs
+++ ChapterFive_AllProject/.AutoProps/Program.cs
@@ -1,2 +1,11 @@
 ﻿using System;
+using AutoProps;
 
+Console.WriteLine("Fun with Auto incapsulation");
+Car c = new Car();
+c.PetName = "Frank";
+c.Speed = 55;
+c.Color = "red";
+Console.WriteLine("Your car has name - {0}", c.PetName);
+c.DisplayStats();
+Console.ReadLine();

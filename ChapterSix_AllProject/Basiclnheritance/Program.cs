460e19d1a7a1e98600b522848262fe3343921dba
Borissov M.R. <shinnn2374@mail.ru>
2025-10-19 17:01:03 +0300

    take Error

--- ChapterSix_AllProject/Basiclnheritance/Program.cs
+++ ChapterSix_AllProject/Basiclnheritance/Program.cs
@@ -6,3 +6,12 @@ Car myCar = new Car(80) { Speed = 50 };
 
 Console.WriteLine("My car is going - {0} MPH", myCar.Speed);
 Console.ReadLine();
+
+Console.WriteLine("____________________________________");
+
+MiniVan miniVan = new MiniVan();
+miniVan.Speed = 10;
+Console.WriteLine("My minivan is going - {0} MPH", miniVan.Speed);
+// Ошибка 
+miniVan._currSpeed = 50;
+Console.ReadLine();

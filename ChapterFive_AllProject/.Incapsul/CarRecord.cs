3220fea4f55ad8b799167af5fa41bf02b40b54c8
Borissov M.R. <shinnn2374@mail.ru>
2025-10-19 16:37:06 +0300

    chapter six is start

---
+++ ChapterFive_AllProject/.Incapsul/CarRecord.cs
@@ -0,0 +1,8 @@
+namespace Incapsul;
+
+public class CarRecord
+{
+    private int id;
+    private string name;
+    private string description;
+}
--- ChapterFive_AllProject/.Incapsul/Program.cs
+++ ChapterFive_AllProject/.Incapsul/Program.cs
@@ -1,7 +1,1 @@
-﻿using System;
-using Incapsul;
-
-Console.WriteLine("Fun with incapsulation");
-Employee emp = new Employee("Marvin", 456, 30000);
-Console.WriteLine(emp.Id.ToString());
-Console.ReadLine();
+﻿

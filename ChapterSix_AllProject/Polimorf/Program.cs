f0d69c2e346189bb827aa6624bf5eef100ea55ff
Borissov M.R. <shinnn2374@mail.ru>
2025-10-19 17:20:58 +0300

    fix

--- ChapterSix_AllProject/Polimorf/Program.cs
+++ ChapterSix_AllProject/Polimorf/Program.cs
@@ -4,5 +4,5 @@ using Employee;
 Console.WriteLine("The Employee class hierarhy");
 Salesperson fred = new Salesperson
 {
-    Age = 31, Name = "Fred", SalesNumber = 50;
+    Age = 31, Name = "Fred", SalesNumber = 50
 };

0e9b669f9b7403f34f2ffc6b0f9af11a834b619a
Borissov M.R. <shinnn2374@mail.ru>
2025-10-22 05:15:27 +0300

    create class Car

--- ChapterSeven_AllProject/SimpleException/Radio.cs
+++ ChapterSeven_AllProject/SimpleException/Radio.cs
@@ -9,4 +9,51 @@ namespace SimpleException
             Console.WriteLine(on ? "Jamming ..." : "Quite time..");
         }
     }
+
+
+    class Car
+    {
+        public const int MaxSpeed = 100;
+
+        public int CurrentSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+
+        // ”казывает не вышел ли автомобиль из стро€?
+        private bool _carIsDead;
+
+        private readonly Radio _theMusicBox = new Radio();
+
+        public Car() { }
+        public Car(string name, int speed) 
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+
+        public void CruncTunes(bool State)
+        {
+            _theMusicBox.TurnOn(State);
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (_carIsDead)
+            {
+                Console.WriteLine("{0} is out of order ...", PetName);
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    Console.WriteLine("{0} has overheated!", PetName);
+                    CurrentSpeed = 0;
+                    _carIsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine("=> CurrentSpedd = {0} ", CurrentSpeed);
+                }
+            }
+        }
 }

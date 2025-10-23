6e519eb02247c5992463b17e18bcbc4b8b3bd937
LeftEye2374 <borissov.mikhail@mail.ru>
2025-10-23 20:57:02 +0300

    weekend

---
+++ SimpleCSharpApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleCSharpConsoleApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "My console app";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("****************************************************");
+            Console.WriteLine("Welcome to my console app");
+            Console.WriteLine("****************************************************");
+            Console.ReadLine();
+        }
+    }
+}

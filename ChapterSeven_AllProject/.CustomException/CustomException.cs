9a21b441b56e320f62a26ef52e6bb66381848906
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-10-26 17:55:00 +0300

    ...

--- ChapterSeven_AllProject/.CustomException/CustomException.cs
+++ ChapterSeven_AllProject/.CustomException/CustomException.cs
@@ -4,4 +4,20 @@ namespace CustomException
     {
         
     }
+
+    public class CarIsDeadException : ApplicationException
+    {
+        private string _messageDetails = String.Empty;
+        public DateTime ErrorTimeStamp { get; set; }
+        public string CauseOfException { get; set; }
+
+        public CarIsDeadException(string message, string cause, DateTime timeStamp)
+        {
+            _messageDetails = message;
+            CauseOfException = cause;
+            ErrorTimeStamp = timeStamp;
+        }
+        
+        public override string Message => $"Car Error: {_messageDetails}";
+    }
 }

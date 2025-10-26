8269016bf07e3a3e6d6bf3d24d3d293836405ccf
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-10-26 18:03:05 +0300

    ...

--- ChapterSeven_AllProject/.CustomException/Program.cs
+++ ChapterSeven_AllProject/.CustomException/Program.cs
@@ -1,1 +1,16 @@
 ﻿using System;
+using CustomException;
+
+Console.WriteLine("Fun with Custom Exception");
+Car myCar = new Car();
+try
+{
+    myCar.Accelerate(50);
+}
+catch (CarIsDeadException ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.CauseOfException);
+    Console.WriteLine(ex.CauseOfException);
+}
+Console.ReadLine();

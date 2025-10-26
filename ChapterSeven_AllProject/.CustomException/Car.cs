2db426dbdd5541801175f9e64681d143b7f40b3f
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-10-26 17:57:59 +0300

    ...

---
+++ ChapterSeven_AllProject/.CustomException/Car.cs
@@ -0,0 +1,47 @@
+namespace CustomException;
+
+public class Car
+{
+    public const int MaxSpeed = 100;
+
+    public int CurrentSpeed { get; set; } = 0;
+    public string PetName { get; set; } = "";
+
+    private bool _carIsDead;
+
+    private readonly Radio _theMusicBox = new Radio();
+
+    public Car() { }
+    public Car(string name, int speed)
+    {
+        CurrentSpeed = speed;
+        PetName = name;
+    }
+
+    public void CruncTunes(bool State)
+    {
+        _theMusicBox.TurnOn(State);
+    }
+
+    public void Accelerate(int delta)
+    {
+        if (_carIsDead) 
+        {
+            Console.WriteLine("{0} is out of order...", PetName);
+        }
+        else
+        {
+            CurrentSpeed += delta;
+            if( CurrentSpeed > MaxSpeed)
+            {
+                CurrentSpeed = 0;
+                _carIsDead = true;
+                throw new CarIsDeadException($"{PetName} has overheated!", "You have a lead foot", DateTime.Now)
+                {
+                    HelpLink = "www.CarPlus.ru",
+                };
+            }
+            Console.WriteLine("Current speed = {0}", CurrentSpeed);
+        }
+    }
+}
---
+++ ChapterSeven_AllProject/.CustomException/Radio.cs
@@ -0,0 +1,9 @@
+namespace CustomException;
+
+public class Radio
+{
+    public void TurnOn(bool on)
+    {
+        Console.WriteLine(on ? "Jamming ..." : "Quite time.."); 
+    }
+}

80b15934446835a247e71fd6534b7fd490317e9b
Borissov M.R. <shinnn2374@mail.ru>
2025-10-30 05:28:20 +0300

    Create Hexagon and Circle classes

--- ChapterEight_AllProject/SpecialInterface/Shape.cs
+++ ChapterEight_AllProject/SpecialInterface/Shape.cs
@@ -1,6 +1,6 @@
 namespace Shape
 {
-    public abstract class Shape
+     abstract class Shape
     {
         protected Shape(string name = "NoName") 
         {
@@ -12,6 +12,24 @@ namespace Shape
         public virtual void Draw()
         {
             Console.WriteLine("Inside Shape.Draw()");
+        }
+    }
+
+    class Circle : Shape 
+    {
+        public Circle() { }
+
+        public Circle(string name) : base(name){ }
+    }
+
+    class Hexagon : Shape
+    {
+        public Hexagon() { }
+        public Hexagon(string name) : base(name) { }
+
+        public override void Draw() 
+        {
+            Console.WriteLine("Drawing {0} the Hexagon", PetName);
         }
     }
 }

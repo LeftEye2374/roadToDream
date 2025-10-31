3ff360ad6fedead59672bd006dd28e257559d8b4
Borissov M.R. <shinnn2374@mail.ru>
2025-10-31 14:36:56 +0300

    check on realisation inteface

--- ChapterEight_AllProject/SpecialInterface/Program.cs
+++ ChapterEight_AllProject/SpecialInterface/Program.cs
@@ -5,3 +5,33 @@ Console.WriteLine("Fun with interfaces");
 Hexagon hex = new Hexagon();
 Console.WriteLine("Points of hexagon: {0}", hex.Points);
 Console.ReadLine();
+
+Console.WriteLine("______________________");
+
+Circle c = new Circle();
+IPointy itpPt;
+try
+{
+    itpPt = (IPointy) c;
+    Console.WriteLine(itpPt.Points);
+}
+catch(InvalidCastException e) 
+{
+    Console.WriteLine(e.Message);
+}
+Console.ReadLine();
+
+Console.WriteLine("______________________");
+
+Hexagon hex2  = new Hexagon("Peter");
+IPointy itpPt2 = hex2 as IPointy;
+if (itpPt2 != null)
+{
+    Console.WriteLine("Ipoints: {}", itpPt2.Points);
+}
+else
+{
+    Console.WriteLine("OOOPS");
+    Console.ReadLine();
+}
+

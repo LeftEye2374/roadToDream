7dd9624565ab1646346873d289eca3be86fb0eb4
Borissov M.R. <shinnn2374@mail.ru>
2025-11-02 17:14:36 +0300

    work with object links

--- ChapterNine_AllProject/SimpleGS/Program.cs
+++ ChapterNine_AllProject/SimpleGS/Program.cs
@@ -1,1 +1,14 @@
 ﻿using System;
+using SimpleGS;
+
+Console.WriteLine("GS basic");
+Car refToMyCar = new Car("Zippy",50);
+Console.WriteLine(refToMyCar.ToString());
+Console.ReadLine();
+
+
+static void MakeACar()
+{
+    Car myCar = new Car();
+    myCar = null;
+}

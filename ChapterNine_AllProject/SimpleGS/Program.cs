dec94cee26bc00d7cf68a4caae5cbbaa655d9926
Borissov M.R. <shinnn2374@mail.ru>
2025-11-03 15:34:51 +0300

    garbage gen

--- ChapterNine_AllProject/SimpleGS/Program.cs
+++ ChapterNine_AllProject/SimpleGS/Program.cs
@@ -6,5 +6,25 @@ Console.WriteLine("Estimated bytes on heap: {0}", GC.GetTotalMemory(false));
 Console.WriteLine("This OS has {0} object generations. \n", (GC.MaxGeneration + 1));
 Car refToMyCar = new Car("Zippy", 100);
 Console.WriteLine(refToMyCar.ToString());
-Console.WriteLine("Generation of refToMyCar is: {0}", GC.GetGeneration(refToMyCar));
+Console.WriteLine("G\n Generation of the refToMyCar is: {0}", GC.GetGeneration(refToMyCar));
+object[] tonsOfObjects = new object[50000];
+for (int i = 0; i < 50000; i++)
+{
+    tonsOfObjects[i] = new object();
+}
+Console.WriteLine("Force Garbage collector");
+GC.Collect(0, GCCollectionMode.Forced);
+GC.WaitForPendingFinalizers();
+Console.WriteLine("Generation of the refToMyCar is: {0}", GC.GetGeneration(refToMyCar));
+if(tonsOfObjects[9000] != null)
+{
+    Console.WriteLine("Generation of the tonsOfObject[9000] is: {0}", GC.GetGeneration(tonsOfObjects[9000]));
+}
+else
+{
+    Console.WriteLine("tonsOfObject[9000] is no longer alive");
+}
+Console.WriteLine("\n Gen 0 has been swept {0} times", GC.CollectionCount(0));
+Console.WriteLine("\n Gen 1 has been swept {0} times", GC.CollectionCount(1));
+Console.WriteLine("\n Gen 2 has been swept {0} times", GC.CollectionCount(2));
 Console.ReadLine();

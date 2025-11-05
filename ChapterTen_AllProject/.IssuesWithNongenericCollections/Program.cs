88e4da61001e4a4db0d9798d4944bb4cb467ae84
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-11-05 18:09:10 +0300

    box / unbox processed

--- ChapterTen_AllProject/.IssuesWithNongenericCollections/Program.cs
+++ ChapterTen_AllProject/.IssuesWithNongenericCollections/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+
+static void SimpleBoxUnboxOperation()
+{
+    int myInt = 25;
+    object boxeInt = myInt;
+    int unboxedInt = (int) boxeInt;
+}

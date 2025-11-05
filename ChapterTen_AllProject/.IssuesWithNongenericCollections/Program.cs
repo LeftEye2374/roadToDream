e1da2e1b31c45a0352dd314cc69c2744e5c9cb1e
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-11-05 18:14:22 +0300

    work with arrayList

--- ChapterTen_AllProject/.IssuesWithNongenericCollections/Program.cs
+++ ChapterTen_AllProject/.IssuesWithNongenericCollections/Program.cs
@@ -1,9 +1,22 @@
 ﻿using System;
 using System.Collections;
 
+WorkWithArrayList();
+
 static void SimpleBoxUnboxOperation()
 {
     int myInt = 25;
     object boxeInt = myInt;
     int unboxedInt = (int) boxeInt;
 }
+
+static void WorkWithArrayList()
+{
+    ArrayList myInts = new ArrayList();
+    myInts.Add(10);
+    myInts.Add(20);
+    myInts.Add(35);
+    int i = (int) myInts[0];
+    Console.WriteLine(i);
+}
+

9c8b5ecfb7e46a2416ee7520f932d88a96a45001
Borissov M.R. <shinnn2374@mail.ru>
2025-11-07 14:14:24 +0300

    create class Person

--- ChapterTen_AllProject/.IssuesWithNongenericCollections/Persion.cs
+++ ChapterTen_AllProject/.IssuesWithNongenericCollections/Persion.cs
@@ -1,4 +1,24 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace IssuesWithNonGenericCollections
 {
+    public class Person
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
+        public Person() { }
+        public Person(int age,  string firstName, string lastName)
+        {
+            Age = age;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {FirstName}{LastName} with age - {Age} ";
+        }
+    }
 }

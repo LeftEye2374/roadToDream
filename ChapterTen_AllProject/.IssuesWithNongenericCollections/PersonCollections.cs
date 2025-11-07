d3954781b848d713117d3c875ff63967847d8aa8
Borissov M.R. <shinnn2374@mail.ru>
2025-11-07 14:22:19 +0300

    create class PersonCollection

---
+++ ChapterTen_AllProject/.IssuesWithNongenericCollections/PersonCollections.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+
+namespace IssuesWithNonGenericCollections 
+{
+    public class PersonCollection : IEnumerable
+    {
+        private ArrayList arPerson = new ArrayList();
+
+        public Person GetPerson(int pos) => (Person)arPerson[pos];
+
+        public void AddPerson(Person p)
+        {
+            arPerson.Add(p);
+        }
+
+        public void ClearPeople()
+        {
+            arPerson.Clear();
+        }
+
+        public int Count => arPerson.Count;
+
+        IEnumerator IEnumerable.GetEnumerator() => arPerson.GetEnumerator();
+    }
+}

f81360d38f81f313a7c48cf9c8f008b06f16eed5
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-11-08 17:46:46 +0300

    ...

---
+++ ChapterTen_AllProject/.GenericPoint/Point.cs
@@ -0,0 +1,34 @@
+namespace GenericPoint
+{
+    public struct Point<T>
+    {
+        private T _xPos;
+        private T _yPos;
+
+        public Point(T x, T y)
+        {
+            _xPos = x;
+            _yPos = y;
+        }
+
+        public T X
+        {
+            get => _xPos;
+            set => _xPos = value;
+        }
+
+        public T Y
+        {
+            get => _yPos;
+            set => _yPos = value;
+        }
+
+        public override string ToString() => $"[{_xPos}, {_yPos}]";
+
+        public void ResetPoint()
+        {
+            _xPos = default(T);
+            _yPos = default(T);
+        }
+    }
+}
--- ChapterTen_AllProject/.GenericPoint/Program.cs
+++ ChapterTen_AllProject/.GenericPoint/Program.cs
@@ -1,25 +1,20 @@
 ﻿using System;
+using GenericPoint;
 
+Console.WriteLine("Fun With Generic Structure");
+Point<int> p = new Point<int>(10, 10);
+Console.WriteLine("p.ToString() = {0}", p.ToString());
+p.ResetPoint();
+Console.WriteLine("p.ToString() = {0}", p.ToString());
+Console.WriteLine();
+Point<double> p2 = new Point<double>(5.4, 3.3);
+Console.WriteLine("p2.ToString() = {0}", p2.ToString());
+p2.ResetPoint();
+Console.WriteLine("p2.ToString() = {0}", p2.ToString());
+Console.WriteLine();
+Point<string> p3 = new Point<string>("3i","i");
+Console.WriteLine("p3.ToString() = {0}", p3.ToString());
+p3.ResetPoint();
+Console.WriteLine("p3.ToString() = {0}", p3.ToString());
+Console.ReadLine();
 
-public struct Point<T>
-{
-    private T _xPos;
-    private T _yPos;
-
-    public Point(T x, T y)
-    {
-        _xPos = x;
-        _yPos = y;
-    }
-    public T X
-    {
-        get => _xPos;
-        set => _xPos = value;
-    }
-    public T Y
-    {
-        get => _yPos;
-        set => _yPos = value;
-    }
-    public override string ToString() => $"[{_xPos}, {_yPos}]";
-}

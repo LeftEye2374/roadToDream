4f3de768de841c20bff30248723a1e73f4f033f9
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-11-08 17:35:16 +0300

    ...

---
+++ ChapterTen_AllProject/.GenericPoint/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+public struct Point<T>
+{
+    private T _xPos;
+    private T _yPos;
+
+    public Point(T x, T y)
+    {
+        _xPos = x;
+        _yPos = y;
+    }
+    public T X
+    {
+        get => _xPos;
+        set => _xPos = value;
+    }
+    public T Y
+    {
+        get => _yPos;
+        set => _yPos = value;
+    }
+    public override string ToString() => $"[{_xPos}, {_yPos}]";
+}

1802cef0d5a2b1c8ff19be53c845f49fedb91e42
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-11-08 18:20:36 +0300

    stop working

--- ChapterEleven_AllProject/ChapterEleven_AllProject/Program.cs
+++ ChapterEleven_AllProject/ChapterEleven_AllProject/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using Chap;
 
-Console.WriteLine("Hello, World!");
+
+static void UseGenericListOfPeople()
+{
+    List<Person> myPeople = new List<Person>();
+    myPeople.Add(new Person("Misha","Borissov",21));
+    myPeople.Add(new Person("Sasha","Skibel",20));
+
+    myPeople[0] = new Person("Sonya", "Borissova", 18);
+
+    for (int i = 0; i < myPeople.Count; i++)
+    {
+        Console.WriteLine("Person number: {0}",i);
+        Console.WriteLine("Name: {0} {1}", myPeople[i].FirstName, myPeople[i].LastName);
+        Console.WriteLine("Age: {0}", myPeople[i].Age);
+        Console.WriteLine();
+    }
+}

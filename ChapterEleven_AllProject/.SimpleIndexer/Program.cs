9859fef3eb1aebf8c7b79423d5d180d8d74d25b8
Borissov M.R. <shinnn2374@mail.ru>
2025-11-09 15:37:27 +0300

    work with generic list indexes

--- ChapterEleven_AllProject/.SimpleIndexer/Program.cs
+++ ChapterEleven_AllProject/.SimpleIndexer/Program.cs
@@ -1,18 +1,35 @@
 ﻿using SimpleIndexer;
 
-Console.WriteLine("Fun With Indexes");
+//Console.WriteLine("Fun With Indexes");
 
-PersonCollection myPeople = new PersonCollection();
-myPeople[0] = new Person("Misha", "Borissov", 21);
-myPeople[1] = new Person("Sonya", "Borissova", 20);
-myPeople[2] =  new Person("Sasha", "Skibel", 18);
-myPeople[3] = new Person("Misha", "Kashanin", 22);
-myPeople[4] = new Person("Alina","Bond",20);
+//PersonCollection myPeople = new PersonCollection();
+//myPeople[0] = new Person("Misha", "Borissov", 21);
+//myPeople[1] = new Person("Sonya", "Borissova", 20);
+//myPeople[2] =  new Person("Sasha", "Skibel", 18);
+//myPeople[3] = new Person("Misha", "Kashanin", 22);
+//myPeople[4] = new Person("Alina","Bond",20);
 
-for (int i = 0; i < myPeople.Count; i++)
+//for (int i = 0; i < myPeople.Count; i++)
+//{
+//    Console.WriteLine("Person number: {0}", i);
+//    Console.WriteLine("Name: {0} {1}", myPeople[i].FirstName, myPeople[i].LastName );
+//    Console.WriteLine("Age: {0}", myPeople[i].Age);
+//    Console.WriteLine();
+//}
+
+static void UseGenericListOfPeople()
 {
-    Console.WriteLine("Person number: {0}", i);
-    Console.WriteLine("Name: {0} {1}", myPeople[i].FirstName, myPeople[i].LastName );
-    Console.WriteLine("Age: {0}", myPeople[i].Age);
-    Console.WriteLine();
+    List<Person> myPeople = new List<Person>();
+    myPeople.Add(new Person("Misha", "Borissov", 21));
+    myPeople.Add(new Person("Sonya", "Borissova", 18));
+    myPeople.Add(new Person("Sasha", "Skibel", 20));
+
+    myPeople[0] = new Person("Alina", "Bond", 20);
+    for (int i =0; i <= myPeople.Count; i++)
+    {
+        Console.WriteLine("Person number: {0}", i);
+        Console.WriteLine("Name: {0} {1}", myPeople[i].FirstName, myPeople[i].LastName);
+        Console.WriteLine("Age: {0}", myPeople[i].Age);
+        Console.WriteLine();
+    }
 }

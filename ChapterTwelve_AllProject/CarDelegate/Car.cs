f9b3779526332675375b8f7d52c29e43ce2650d8
Borissov M.R. <shinnn2374@mail.ru>
2025-11-10 17:17:24 +0300

    Create delegates for Car.cs

--- ChapterTwelve_AllProject/CarDelegate/Car.cs
+++ ChapterTwelve_AllProject/CarDelegate/Car.cs
@@ -8,11 +8,22 @@ namespace CarDelegate
 
         private bool _carIsDead;
 
-        public Car() {}
-        public Car(string petName,  int maxSp, int currSp)
+        public Car() { }
+        public Car(string petName, int maxSp, int currSp)
         {
             PetName = petName;
             MaxSpeed = maxSp;
             CurrentSpeed = currSp;
         }
+
+        // 1. Определить тип делегата
+        public delegate void CarEngineHandler(string msgForCaller);
+        // 2. Определить переменную-член этого типа делегата
+        public CarEngineHandler _listOfHandlers;
+        // 3. Добавить регистрационную функцию для вызывающего кода
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            _listOfHandlers = methodToCall;
+        }
+    }
 }

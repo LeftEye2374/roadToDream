0c21a599ec775fc1828f8df73cd7688e8ff0c351
Borissov M.R. <shinnn2374@mail.ru>
2025-11-11 13:27:23 +0300

    Approve

--- ChapterTwelve_AllProject/PublicDelegateProblem/Program.cs
+++ ChapterTwelve_AllProject/PublicDelegateProblem/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using PublicDelegateProblem;
+
+Console.WriteLine("Fun With No Encapsulation");
+Car myCar = new Car();
+myCar.ListOfHandlers = CallWithExploded;
+myCar.Accelerate(10);
+myCar.ListOfHandlers = CallHereToo;
+myCar.Accelerate(10);
+myCar.ListOfHandlers.Invoke("Hee, hee, hee");
+
+Console.ReadLine();
+
+static void CallWithExploded(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+static void CallHereToo(string msg)
+{
+    Console.WriteLine(msg);
+}
+

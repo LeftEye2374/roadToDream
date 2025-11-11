52b58d08febbd9c6ebf68e8aeec63474db93cc5d
Borissov M.R. <shinnn2374@mail.ru>
2025-11-11 13:20:15 +0300

    PublicDelegateProblem

--- ChapterTwelve_AllProject/ActionAndFuncDelegates/Program.cs
+++ ChapterTwelve_AllProject/ActionAndFuncDelegates/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+Console.WriteLine("Fun With Action And Func");
+Action<string, ConsoleColor, int> actionTarget = DisplayMessage;
+actionTarget("Action Message!", ConsoleColor.Yellow, 5);
+Console.ReadLine();
+
+
+
+static void DisplayMessage(string msg, ConsoleColor txtColor, int printCount)
+{
+    ConsoleColor previus = Console.ForegroundColor;
+    Console.ForegroundColor = txtColor;
+    for(int i = 0; i < printCount; i++)
+    {
+        Console.WriteLine(msg);
+    }
+    Console.ForegroundColor = previus;
+}
+
+static int Add(int x, int y)
+{
+    return x + y;
+}

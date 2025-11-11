dfb27e44295347b6a47169bde9c569b47aa34ec8
Borissov M.R. <shinnn2374@mail.ru>
2025-11-11 12:55:10 +0300

    ...

--- ChapterTwelve_AllProject/CarDelegate/Program.cs
+++ ChapterTwelve_AllProject/CarDelegate/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using CarDelegate;
+
+Console.WriteLine("Delegate as event enablers");
+
+Car c1 = new Car("SlugBag", 100, 10);
+c1.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent));
+Console.WriteLine("Speeding up");
+for(int i =0; i < 6; i++)
+{
+    c1.Accelerate(20);
+}
+Console.ReadLine();
+
+
+static void OnCarEngineEvent(string msg)
+{
+    Console.WriteLine("Message From Car  Object");
+    Console.WriteLine("=> {0}", msg);
+    Console.WriteLine("*********************************");
+}

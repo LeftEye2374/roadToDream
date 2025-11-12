431204469acfe342d469256e876434a297593f3b
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-11-12 18:49:23 +0300

    LINQ for arrays

--- ChapterThirteen_AllProject/LinqOverArray/Program.cs
+++ ChapterThirteen_AllProject/LinqOverArray/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.WriteLine("Fun With LINQ Object");
+QueryOverStrings();
+Console.ReadLine();
+
+static void QueryOverStrings()
+{
+    string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "BioShock 2" };
+    
+    IEnumerable<string> subset = from g in currentVideoGames where g.Contains(" ") orderby g select g;
+    foreach (string s in subset )
+    {
+        Console.WriteLine(s);
+    }
+}

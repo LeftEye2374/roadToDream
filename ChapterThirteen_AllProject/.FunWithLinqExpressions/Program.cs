1ec72a53c531e5cbdc4e9040fc3daec8e7c03eec
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-11-13 21:02:41 +0300

    some queryes

--- ChapterThirteen_AllProject/.FunWithLinqExpressions/Program.cs
+++ ChapterThirteen_AllProject/.FunWithLinqExpressions/Program.cs
@@ -10,4 +10,35 @@ ProductInfo[] itemInStock = new[]
     new ProductInfo{Name = "cake", Description = "blue cake", NumberInStock = 12},
     new ProductInfo{Name = "pizza", Description = "pizza", NumberInStock = 11},
 };
+MediumLvlQuery(itemInStock);
 Console.ReadLine();
+
+static void SelectEvrething(ProductInfo[] products)
+{
+    Console.WriteLine("All product details");
+    var allProduct = from product in products select product;
+    foreach (var product in allProduct)
+    {
+        Console.WriteLine(product.ToString());
+    }
+}
+
+static void SelectPorductNames(ProductInfo[] products)
+{
+    Console.WriteLine("All product Names");
+    var allProductNames = from p in products select p.Name;
+    foreach (var product in allProductNames)
+    {
+        Console.WriteLine(product);
+    }
+}
+
+static void MediumLvlQuery(ProductInfo[] products)
+{
+    Console.WriteLine("Medium level query");
+    var query = from p in products where p.NumberInStock > 15 select p.Name;
+    foreach (var product in query)
+    {
+        Console.WriteLine(product);
+    }
+}

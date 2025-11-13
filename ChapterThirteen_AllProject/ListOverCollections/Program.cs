762dfc4f38ebce203bf2799eb9a7024f8e99fc9a
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-11-13 20:36:05 +0300

    LINQ for object collection

--- ChapterThirteen_AllProject/ListOverCollections/Program.cs
+++ ChapterThirteen_AllProject/ListOverCollections/Program.cs
@@ -1,1 +1,14 @@
 ﻿using System;
+using ListOverCollections;
+
+Console.WriteLine("LINQ over Generics Collection");
+List<Car> myCar = new List<Car>()
+{
+    new Car {PetName = "Henry", Color = "Silver", Speed = 100, Make = "BMW"},
+    new Car {PetName = "Daisy", Color = "Tan", Speed = 90, Make = "BMW"},
+    new Car {PetName = "Mary", Color = "Black", Speed = 55, Make = "VM"},
+    new Car {PetName = "Clunker", Color = "Rust", Speed = 5, Make = "Yugo"},
+    new Car {PetName = "Melvin", Color = "White", Speed = 43, Make = "Ford"}
+};
+Console.ReadLine();
+

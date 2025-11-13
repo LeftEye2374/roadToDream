8ba5392e7fa832152b707dad1d2189ed12b13f07
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-11-13 04:25:43 +0300

    return values of LINQ

---
+++ ChapterThirteen_AllProject/LinqOverArray/LINQBasedFieldsAreClunky.cs
@@ -0,0 +1,16 @@
+namespace LINQBasedFieldsAreClunky
+{
+    public class LINQBasedFieldsAreClunky
+    {
+        private static string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2" };
+        private IEnumerable<string> subset = from g in currentVideoGames where g.Contains(" ") orderby g select g;
+
+        public void PrintGames()
+        {
+            foreach (var item in subset)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- ChapterThirteen_AllProject/LinqOverArray/Program.cs
+++ ChapterThirteen_AllProject/LinqOverArray/Program.cs
@@ -1,35 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-Console.WriteLine("Fun With LINQ Object");
-QueryOverInts();
+Console.WriteLine("Linq return values");
+IEnumerable<string> subset = GetStringsSubset();
+foreach( string item in subset)
+{
+    Console.WriteLine(item);
+}
 Console.ReadLine();
 
-static void QueryOverStrings()
+static IEnumerable<string> GetStringsSubset()
 {
-    string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "BioShock 2" };
-    
-    IEnumerable<string> subset = currentVideoGames.Where(g => g.Contains(" ")).OrderBy(g => g).Select(g => g);
-    ReflectOverQueryResults(subset, "Extension Methods");
-    foreach (string s in subset )
-    {
-        Console.WriteLine(s);
-    }
-}
-
-static void ReflectOverQueryResults(object resultSet, string queryType ="Query Expressions")
-{
-    Console.WriteLine($"Info about your query using {queryType}");
-    Console.WriteLine("resulSet is of type: {0}", resultSet.GetType().Name);
-    Console.WriteLine("resulSet location: {0}", resultSet.GetType().Assembly.GetName().Name);
-}
-
-static void QueryOverInts()
-{
-    int[] numbers = { 10,20,30,40,1,2,3,8 };
-    IEnumerable<int> subset = from i in numbers where i < 10 select i;
-    foreach(int i in subset)
-    {
-        Console.WriteLine("Item: {0}", i);
-    }
-    ReflectOverQueryResults(subset);
+    string[] colors = {"Light Red", "Green","Yellow", "Dark Red", "Red", "Purple"};
+    IEnumerable<string> theRedColors = from c in colors where c.Contains("Red") select c;
+    return theRedColors;
 }

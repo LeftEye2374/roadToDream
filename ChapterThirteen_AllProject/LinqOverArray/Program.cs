40e3767a508a80ef62e9d1f224c37ff6f909aa38
Borissov M.R. <shinnn2374@mail.ru>
2025-11-13 03:50:39 +0300

    Linq for arrays reflect

--- ChapterThirteen_AllProject/LinqOverArray/Program.cs
+++ ChapterThirteen_AllProject/LinqOverArray/Program.cs
@@ -9,8 +9,16 @@ static void QueryOverStrings()
     string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "BioShock 2" };
     
     IEnumerable<string> subset = from g in currentVideoGames where g.Contains(" ") orderby g select g;
+    ReflectOverQueryResults(subset);
     foreach (string s in subset )
     {
         Console.WriteLine(s);
     }
+}
+
+static void ReflectOverQueryResults(object resultSet, string queryType ="Query Expressions")
+{
+    Console.WriteLine($"Info about your query using {queryType}");
+    Console.WriteLine("resulSet is of type: {0}", resultSet.GetType().Name);
+    Console.WriteLine("resulSet location: {0}", resultSet.GetType().Assembly.GetName().Name);
 }

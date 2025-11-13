352570a8a75719da0fe216687a0278f226eda8ba
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-11-13 20:56:26 +0300

    fun with query expressions

--- ChapterThirteen_AllProject/.FunWithLinqExpressions/Program.cs
+++ ChapterThirteen_AllProject/.FunWithLinqExpressions/Program.cs
@@ -1,1 +1,13 @@
 ﻿using System;
+using FunWithLinqExpressions;
+
+Console.WriteLine("Fun with Query Expressions");
+ProductInfo[] itemInStock = new[]
+{
+    new ProductInfo{Name = "Mac", Description = "Coffee", NumberInStock = 24},
+    new ProductInfo{Name = "Macbook", Description = "Laptop", NumberInStock = 31},
+    new ProductInfo{Name = "Milk", Description = "white water", NumberInStock = 23},
+    new ProductInfo{Name = "cake", Description = "blue cake", NumberInStock = 12},
+    new ProductInfo{Name = "pizza", Description = "pizza", NumberInStock = 11},
+};
+Console.ReadLine();

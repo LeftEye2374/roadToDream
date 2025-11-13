4d6f0f40120537e8895721223692fc6ead4e5771
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-11-14 02:42:08 +0300

    Fun With Processes

---
+++ ChapterFourteen_AllProject/.ProcessManipulator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+Console.WriteLine("**** Fun With Processes ****");
+ListAllRunningProcesses();
+Console.ReadLine();
+
+
+static void ListAllRunningProcesses()
+{
+    var runningProcs = from proc in Process.GetProcesses(".") orderby  proc.Id select proc;
+    foreach (var p in runningProcs)
+    {
+        string info = $"-> PID: {p.Id}\t Name: {p.ProcessName}";
+        Console.WriteLine(info);
+    }
+
+    Console.WriteLine("***********************************");
+}

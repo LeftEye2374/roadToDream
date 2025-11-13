9f3ddef39da4def815b9753cca6fa4c28ab4e43a
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-11-14 02:55:22 +0300

    Some methods for use processes

--- ChapterFourteen_AllProject/.ProcessManipulator/Program.cs
+++ ChapterFourteen_AllProject/.ProcessManipulator/Program.cs
@@ -3,6 +3,14 @@ using System.Diagnostics;
 
 Console.WriteLine("**** Fun With Processes ****");
 ListAllRunningProcesses();
+GetSpecificProcess();
+
+Console.WriteLine("Enter PID of process ");
+Console.Write("PID: ");
+string pid = Console.ReadLine();
+int pId = int.Parse(pid);
+EnumThreadsForPid(pId);
+
 Console.ReadLine();
 
 
@@ -15,5 +23,41 @@ static void ListAllRunningProcesses()
         Console.WriteLine(info);
     }
 
+    Console.WriteLine("***********************************");
+}
+
+static void GetSpecificProcess()
+{
+    Process theProc = null;
+    try
+    {
+        theProc = Process.GetProcessById(20466);
+    }
+    catch (ArgumentException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    Console.WriteLine("***********************************");
+}
+
+static void EnumThreadsForPid(int pID)
+{
+    Process theProc = null;
+    try
+    {
+        theProc = Process.GetProcessById(pID);
+    }
+    catch (ArgumentException ex)
+    {
+        Console.WriteLine(ex.Message);
+        return;
+    }
+    Console.WriteLine("Here are threads used by: {0}", theProc.ProcessName);
+    ProcessThreadCollection theThreads = theProc.Threads;
+    foreach (ProcessThread pt in theThreads)
+    {
+        string info = $"-> Thread ID: {pt.Id}\t Start time: {pt.StartTime.ToShortTimeString()}\t Priority: {pt.PriorityLevel} ";
+        Console.WriteLine(info);
+    }
     Console.WriteLine("***********************************");
 }

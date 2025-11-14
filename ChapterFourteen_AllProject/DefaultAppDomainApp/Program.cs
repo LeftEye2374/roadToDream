0394f16c40d1896d411ef5ee51791da8b97428a1
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-11-14 15:01:34 +0300

    start work with domain

---
+++ ChapterFourteen_AllProject/DefaultAppDomainApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+
+Console.WriteLine("Fun with Domain");
+DisplayDADStats();
+Console.ReadLine();
+
+
+static void DisplayDADStats()
+{
+    AppDomain defaultDM = AppDomain.CurrentDomain;
+    Console.WriteLine("Name of this Domain: {0}", defaultDM.FriendlyName);
+    Console.WriteLine("ID of this Domain: {0}", defaultDM.Id);
+    Console.WriteLine("Is this default domain? {0}", defaultDM.IsDefaultAppDomain());
+    Console.WriteLine("Base directory of this domain: {0}", defaultDM.BaseDirectory);
+    Console.WriteLine("Setup information of this domain: {0}", defaultDM.SetupInformation);
+
+}

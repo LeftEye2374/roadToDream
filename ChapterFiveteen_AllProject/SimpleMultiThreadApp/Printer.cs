d615e23ab06f282226c4e70b3394f632e57192fa
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-11-14 18:04:36 +0300

    second thread

---
+++ ChapterFiveteen_AllProject/SimpleMultiThreadApp/Printer.cs
@@ -0,0 +1,17 @@
+namespace SimpleMultiThreadApp
+{
+    public class Printer
+    {
+        public void PrintNumbers()
+        {
+            Console.WriteLine("--> {0} is executing PrintNumbers()", Thread.CurrentThread.Name);
+            Console.WriteLine("Your numbers: ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("{0}, ",i);
+                Thread.Sleep(2000);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ChapterFiveteen_AllProject/SimpleMultiThreadApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using SimpleMultiThreadApp;
+
+Console.WriteLine("Amazing Thread App");
+Console.WriteLine("Do you want [1] or [2] threads?");
+string threadCount = Console.ReadLine();
+Thread primareyThread = Thread.CurrentThread;
+primareyThread.Name = "Primary";
+Console.WriteLine("--> {0} is executing Main()", Thread.CurrentThread.Name);
+
+Printer p = new Printer();
+switch (threadCount)
+{
+    case "2":
+        Thread backGroundThread = new Thread(new ThreadStart(p.PrintNumbers));
+        backGroundThread.Name = "Secondary";
+        backGroundThread.Start();
+        break;
+    case "1":
+        p.PrintNumbers();
+        break;
+    default:
+        Console.WriteLine("I dont know what you want");
+        goto case "1";
+}
+
+Console.ReadLine();

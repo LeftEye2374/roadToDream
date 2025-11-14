1732caf6f5cca2ea1143a3c2339103cb7dc63bea
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-11-14 18:21:16 +0300

    waitOne()

--- ChapterFiveteen_AllProject/AddWithThreads/Program.cs
+++ ChapterFiveteen_AllProject/AddWithThreads/Program.cs
@@ -2,11 +2,16 @@
 using System.Threading;
 using AddWithThreads;
 
+AutoResetEvent _waitHandle = new AutoResetEvent(false);
+
 Console.WriteLine("Adding with Thread object");
 Console.WriteLine("ID of thread Main(): {0}", Thread.CurrentThread.ManagedThreadId);
 AddParams ap = new AddParams(10, 10);
 Thread t = new Thread(new ParameterizedThreadStart(Add));
 t.Start(ap);
+
+//Ождиать пока не наступит уведомление 
+_waitHandle.WaitOne();
 Thread.Sleep(5);
 Console.ReadLine();
 
@@ -17,5 +22,7 @@ static void Add(object data)
     {
         Console.WriteLine("ID of thread in Add(): {0}", Thread.CurrentThread.ManagedThreadId);
         Console.WriteLine("{0} + {1} = {2}", ap.a, ap.b, ap.a + ap.b);
+        // Сообщит другому потоку что работа завершена
+        _waitHandle.Set();
     }
 }

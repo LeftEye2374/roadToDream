3545288c08f055105051de454c778e33ddc57dd0
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-11-14 15:38:21 +0300

    work with threads

---
+++ ChapterFiveteen_AllProject/ThreadStats/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+
+Console.WriteLine("Primary Thread Stats");
+Thread primaryThread = Thread.CurrentThread;
+primaryThread.Name = "ThePrimaryThread";
+
+Console.WriteLine("Thread name: {0}", primaryThread.Name);
+Console.WriteLine("Has threads started? {0}", primaryThread.IsAlive);
+Console.WriteLine("Priority Level: {0}", primaryThread.Priority);
+Console.WriteLine("Thread state: {0}", primaryThread.ThreadState);
+Console.ReadLine();

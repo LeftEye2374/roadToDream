49d94a8965f29b9730e8b42cd6bf829d3db88e3a
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-11-15 13:36:24 +0300

    racing example

---
+++ ChapterFiveteen_AllProject/MultiThreadedPrinting/Printer.cs
@@ -0,0 +1,17 @@
+namespace MultiThreadedPrinting
+{
+    public class Printer
+    {
+        public void PrintNumbers()
+        {
+            Console.WriteLine("--> {0} is executing PrintNumbers()", Thread.CurrentThread.Name);
+            for( int i =0; i < 10; i++)
+            {
+                Random r = new Random();
+                Thread.Sleep(1000 * r.Next(5));
+                Console.WriteLine("{0},",i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ChapterFiveteen_AllProject/MultiThreadedPrinting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using MultiThreadedPrinting;
+Console.WriteLine("Synchronized Threads");
+
+Printer p = new Printer();
+Thread[] threads = new Thread[10];
+
+for (int i = 0; i < 10; i++)
+{
+    threads[i] = new Thread(new ThreadStart(p.PrintNumbers))
+    {
+        Name = $"Worker thread #{i}"
+    };    
+}
+
+foreach( Thread t in threads)
+{
+    t.Start();
+}
+Console.ReadLine();

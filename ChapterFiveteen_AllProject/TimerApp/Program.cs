ecfa409a0e54ce342971ca8795298d2c51975d72
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-11-15 14:02:02 +0300

    timerCallBack

---
+++ ChapterFiveteen_AllProject/TimerApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+
+Console.WriteLine("Working with timer type");
+TimerCallback timerCB = new TimerCallback(PrintTime);
+
+Timer t = new Timer(timerCB,null,0,1000);
+Console.WriteLine("Hit key to terminate");
+Console.ReadLine();
+static void PrintTime(object state)
+{
+    Console.WriteLine("Time is: {0}", DateTime.Now.ToLongDateString());
+}

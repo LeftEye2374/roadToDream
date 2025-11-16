fb2cea50f839ea7578f7698887e501ff0da863e1
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-11-16 16:40:35 +0300

    DisplaylmageFiles

--- ChapterTwenty_AllProject/.DirectorуАрри/Program.cs
+++ ChapterTwenty_AllProject/.DirectorуАрри/Program.cs
@@ -14,3 +14,19 @@ static void ShowWindowDirectoryInfo()
     Console.WriteLine("Root: {0}", dir.Root);
     Console.WriteLine("******************");
 }
+
+static void DisplayImageFiles()
+{
+    DirectoryInfo dir = new DirectoryInfo($"Window"); // path to dir
+    FileInfo[] imageFile = dir.GetFiles("*.jpg", SearchOption.AllDirectories);
+    Console.WriteLine("Found: {0} *.jpg\n", imageFile.Length );
+    foreach (FileInfo file in imageFile)
+    {
+        Console.WriteLine("************************");
+        Console.WriteLine("Filename: {0}", file.Name);
+        Console.WriteLine("File size: {0}", file.Length);
+        Console.WriteLine("Creation: {0}", file.CreationTime);
+        Console.WriteLine("Attributes: {0}", file.Attributes);
+        Console.WriteLine("************************");
+    }
+}

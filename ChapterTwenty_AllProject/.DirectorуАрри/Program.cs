aa1f38cbf8cd609b71126a0c4e8eaa73e4528eb4
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-11-16 16:35:04 +0300

    ShowWindowDirectoryInfo

---
+++ ChapterTwenty_AllProject/.DirectorуАрри/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Fun with Directory(info)");
+ShowWindowDirectoryInfo();
+Console.ReadLine();
+
+static void ShowWindowDirectoryInfo()
+{
+    DirectoryInfo dir = new DirectoryInfo($"Window"); // path to dir
+    Console.WriteLine("Directory Info: ->");
+    Console.WriteLine("FullName: {0}", dir.FullName);
+    Console.WriteLine("Name: {0}", dir.Name);
+    Console.WriteLine("Parent: {0}", dir.Parent);
+    Console.WriteLine("Creation: {0}", dir.CreationTime);
+    Console.WriteLine("Attributes: {0}", dir.Attributes);
+    Console.WriteLine("Root: {0}", dir.Root);
+    Console.WriteLine("******************");
+}

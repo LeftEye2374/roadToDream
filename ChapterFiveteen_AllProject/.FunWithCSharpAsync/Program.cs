bdf34f3775101b6680e0e66fadd332241dc581b7
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-11-16 14:15:23 +0300

    multiply async

--- ChapterFiveteen_AllProject/.FunWithCSharpAsync/Program.cs
+++ ChapterFiveteen_AllProject/.FunWithCSharpAsync/Program.cs
@@ -1,6 +1,5 @@
 ﻿Console.WriteLine("Fun with async ->");
-Console.WriteLine(DoWorkAsync());
-Console.WriteLine("Completed");
+MultiplyAwait();
 Console.ReadLine();
 
 static string DoWork()
@@ -14,7 +13,26 @@ static async Task<string> DoWorkAsync()
     return await Task.Run(() =>
     {
         Thread.Sleep(5000);
-        Console.WriteLine("Done with work");
         return "Done with work";
     });
+}
+
+static async Task MultiplyAwait()
+{
+    var task1 = Task.Run(() =>
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine("Done with first task!");
+    });
+    var task2 = Task.Run(() =>
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine("Done with second task!");
+    });
+    var task3 = Task.Run(() =>
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine("Done with third task!");
+    });
+    await Task.WhenAll(task1, task2, task3);
 }

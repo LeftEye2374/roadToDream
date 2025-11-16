64b8d0205ff869c33bf526e79fd7f6160e3e0b43
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-11-16 14:00:37 +0300

    async

---
+++ ChapterFiveteen_AllProject/.FunWithCSharpAsync/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Fun with async ->");
+Console.WriteLine(DoWorkAsync());
+Console.WriteLine("Completed");
+Console.ReadLine();
+
+static string DoWork()
+{
+    Thread.Sleep(5000);
+    return "Done with work";
+}
+
+static async Task<string> DoWorkAsync()
+{
+    return await Task.Run(() =>
+    {
+        Thread.Sleep(5000);
+        Console.WriteLine("Done with work");
+        return "Done with work";
+    });
+}

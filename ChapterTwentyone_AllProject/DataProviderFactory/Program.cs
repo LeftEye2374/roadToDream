3ce16f16ca7d25206fc5d5d35756b3a5d3062cdc
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-11-18 15:54:30 +0300

    stop work

--- ChapterTwentyone_AllProject/DataProviderFactory/Program.cs
+++ ChapterTwentyone_AllProject/DataProviderFactory/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Data.Common;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.IO;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using DataProviderFactory;
+
+Console.WriteLine("Fun With Data Provider Factories");
+var (provider, connectionString) = GetProviderFromConfiguration();
+DbProviderFactory factory = GetDbProviderFactory(provider);
+using (DbConnection connection = factory.CreateConnection())
+{
+    if(connection == null)
+    {
+        Console.WriteLine($"Unable to create connectino object");
+        return;
+    }
+    Console.WriteLine($"Your connection object is a: {connection.GetType().Name}");
+    connection.ConnectionString = connectionString;
+    connection.Open();
+    DbCommand command = factory.CreateCommand();
+    if(command == null )
+    {
+        Console.WriteLine("Unable to create command object");
+        return;
+    }
+    Console.WriteLine($"Your command object is a: {command.GetType().Name}");
+    command.Connection = connection;
+    command.CommandText = "Select i.Id, m.Name From Inventory i inner join Makes m on  m.Id= i.MakeId";
+    using (DbDataReader dataReader = command.ExecuteReader())
+    {
+        Console.WriteLine($"Your data reader object is a: {dataReader.GetType().Name}");
+        Console.WriteLine("******************Current Inventory******************");
+        while (dataReader.Read())
+        {
+            Console.WriteLine($"--> Car #{dataReader["Id]} is a {datReader["Name"]}.");
+        }
+    }
+}
+Console.ReadLine();

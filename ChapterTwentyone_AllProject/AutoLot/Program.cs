35022a79382bcfdfcd525021d771536b71ad5c9a
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-11-20 15:33:15 +0300

    Add package Data.SqlClient

--- ChapterTwentyone_AllProject/AutoLot/Program.cs
+++ ChapterTwentyone_AllProject/AutoLot/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+Console.WriteLine("Fun With Data Readers");
+using (SqlConnection connection = new SqlConnection())
+{
+    connection.ConnectionString = @"Data Source=.,5433;User Id=sa;Password=P@ssw0rd;Initial Catalog=AutoLot";
+    connection.Open();
+
+    string sql = @"SELECT i.id, m.Name as Make, i.Color, i.PenName FROM Inventory i INNER JOIN Makes m on m.Id = i.MakeId";
+
+}

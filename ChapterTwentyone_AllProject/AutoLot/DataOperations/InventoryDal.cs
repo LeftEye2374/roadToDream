608bda567984892634fe5fcc8d59575a1e0ae5b3
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-11-22 03:15:29 +0300

    create methods Open/Close Connection

--- ChapterTwentyone_AllProject/AutoLot/DataOperations/InventoryDal.cs
+++ ChapterTwentyone_AllProject/AutoLot/DataOperations/InventoryDal.cs
@@ -8,12 +8,32 @@ namespace AutoLot.Dal.DataOperations
 {
     public class InventoryDal
     {
+        private SqlConnection _sqlConnection = null;
+
         private readonly string _connectionString;
 
         public InventoryDal() : this( @"Data Source=.,5433;User Id=sa;Password=P@ssw0rd;Initial Catalog=AutoLot")
         {
         }
 
-        public InventoryDal(string connectionString) => _connectionString = connectionString; 
+        public InventoryDal(string connectionString) => _connectionString = connectionString;
+
+
+        private void OpenConnection()
+        {
+            _sqlConnection = new SqlConnection
+            {
+                ConnectionString = _connectionString
+            };
+            _sqlConnection.Open();
+        }
+
+        private void CloseConnection()
+        {
+            if( _sqlConnection?.State != ConnectionState.Closed )
+            {
+                _sqlConnection?.Close();
+            }
+        }
     }
 }

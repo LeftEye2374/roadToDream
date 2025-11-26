9b9d391c5fa1890dc88ebf5d8e60a205b6d91d33
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-11-26 16:08:31 +0300

    ShowConnectionStatus

--- ADO.NEW.With.EF/AutoLot.DataReader/Program.cs
+++ ADO.NEW.With.EF/AutoLot.DataReader/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.Data.SqlClient;
 Console.WriteLine("Fun with readres\n");
 using( SqlConnection connection = new SqlConnection())
 {
-    connection.ConnectionString = @"Data Source=.,5433; User Id=sa;Password=P@ssw0rd;Initial Catalog=AutoLot";
+    connection.ConnectionString = @"Data Source=.,5433; User Id=sa;Password=P@ssw0rd;Initial Catalog=AutoLot;Connect Timeout=30";
     connection.Open();
     string sql = "SELECT i.Id, m.Name AS Make, i.Color, i.Petname FROM Inventory i INNER JOIN Makes m on m.Id = i.MakeId";
     SqlCommand myCommand = new SqlCommand(sql, connection);
@@ -16,5 +16,15 @@ using( SqlConnection connection = new SqlConnection())
             Console.WriteLine($"-> Make: {myDataReader["Make"]}, Petname: {myDataReader["PetName"]}, Color: {myDataReader["Color"]}");
         }
     }
-}
+} 
 Console.ReadLine();
+
+
+static void ShowConnectionStatus(SqlConnection connection)
+{
+    Console.WriteLine("Info about your connection");
+    Console.WriteLine($@"Database lcoation: {connection.DataSource}");
+    Console.WriteLine($"Database name: {connection.Database}");
+    Console.WriteLine($@"TimeOut: {connection.ConnectionTimeout}");
+    Console.WriteLine($"Connection state: {connection.State}\n");
+}

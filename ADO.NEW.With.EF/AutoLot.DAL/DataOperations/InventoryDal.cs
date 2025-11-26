353be0b0d08ef545fba17ee7e4cde637b0ff807d
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-11-26 18:09:20 +0300

    add CRUD methods

--- ADO.NEW.With.EF/AutoLot.DAL/DataOperations/InventoryDal.cs
+++ ADO.NEW.With.EF/AutoLot.DAL/DataOperations/InventoryDal.cs
@@ -79,5 +79,72 @@ namespace AutoLot.Dal.DataOperations
             dataReader.Close();
             return inventory;
         }
+
+        public CarViewModel GetCar(int id)
+        {
+            OpenConnection();
+            CarViewModel car = null;
+            string sql = $@"SELECT i.Id, i.Color, i.PetName, m.Make AS Make FROM Inventory i INNER JOIN Makes m ON m.Id = i.MakeId WHERE i.Id = {id}";
+            using SqlCommand command = new SqlCommand(sql, _sqlConnectoin)
+            {
+                CommandType = CommandType.Text
+            };
+            SqlDataReader dataReader = command.ExecuteReader(CommandBehavior.CloseConnection);
+            while (dataReader.Read())
+            {
+                car = new CarViewModel()
+                {
+                    Id = (int)dataReader["Id"],
+                    Color = (string)dataReader["Color"],
+                    Make = (string)dataReader["Make"],
+                    PetName = (string)dataReader["PetName"]
+                };
+            }
+            dataReader.Close();
+            return car;
+        }
+
+        public void InsertAuto(Car car)
+        {
+            OpenConnection();
+            string sql = $@"INSERT INTO Inventory (MakeId, Color, PetName) VALUES ('{car.MakeId}', '{car.Color}', '{car.PetName}')";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnectoin))
+            {
+                command.CommandType = CommandType.Text;
+                command.ExecuteNonQuery();
+            }
+            CloseConnection();
+        }
+
+        public void DeleteCar(int id)
+        {
+            OpenConnection();
+            string sql = $@"DELETE FROM Inventory WHERE Id = '{id}'";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnectoin))
+            {
+                try
+                {
+                    command.CommandType = CommandType.Text;
+                    command.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    Exception error = new Exception("Sorry! That car is on order!", ex);
+                    throw error;
+                }
+            }
+            CloseConnection();
+        }
+
+        public void UpdateCarPetName(int id, string PetName)
+        {
+            OpenConnection();
+            string sql = @$"UPDATE Inventory SET PetName = '{PetName}' WHERE Id = '{id}'";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnectoin))
+            {
+                command.ExecuteNonQuery();
+            }
+            CloseConnection();
+        }
     }
 }

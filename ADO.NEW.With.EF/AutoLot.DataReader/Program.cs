9049d463f4e9de7482e07f16e55ce3cce26417f5
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-11-26 15:28:59 +0300

    ...

--- ADO.NEW.With.EF/AutoLot.DataReader/Program.cs
+++ ADO.NEW.With.EF/AutoLot.DataReader/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+Console.WriteLine("Fun with readres\n");
+using( SqlConnection connection = new SqlConnection())
+{
+    connection.ConnectionString = @"Data Source=.,5433; User Id=sa;Password=P@ssw0rd;Initial Catalog=AutoLot";
+    connection.Open();
+    string sql = "SELECT i.Id, m.Name AS Make, i.Color, i.Petname FROM Inventory i INNER JOIN Makes m on m.Id = i.MakeId";
+    SqlCommand myCommand = new SqlCommand(sql, connection);
+
+    using (SqlDataReader myDataReader = myCommand.ExecuteReader())
+    {
+        while (myDataReader.Read())
+        {
+            Console.WriteLine($"-> Make: {myDataReader["Make"]}, Petname: {myDataReader["PetName"]}, Color: {myDataReader["Color"]}");
+        }
+    }
+}
+Console.ReadLine();

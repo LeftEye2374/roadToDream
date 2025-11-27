2e91af56148415b19c008145cd1062019cf527dc
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-11-28 01:46:28 +0300

    the DataReader is finished

--- ADO.NEW.With.EF/AutoLot.DAL/BulkImport/MyDataReader.cs
+++ ADO.NEW.With.EF/AutoLot.DAL/BulkImport/MyDataReader.cs
@@ -1,29 +1,102 @@
 using AutoLot.Dal.BulkImport;
+using AutoLot.Dal.Models;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Reflection;
-
-public sealed class MyDataReader<T> : IMyDataReader<T>
+namespace AutoLot.Dal.BulkImport
 {
-    private int _currentIndex = -1;
-
-    public List<T> Records { get; set; }
-
-
-    public MyDataReader(List<T> reacords)
+    public sealed class MyDataReader<T> : IMyDataReader<T>
     {
-        Records = reacords;
-    }
+        private int _currentIndex = -1;
+        private readonly PropertyInfo[] _propertyInfos;
+        private readonly Dictionary<int, string> _nameDictionary;
+        private readonly SqlConnection _connection;
+        private readonly string _schema;
+        private readonly string _tableName;
+        public List<T> Records { get; set; }
 
-    public bool Read()
-    {
-        if (_currentIndex + 1 >= Records.Count)
+
+        public MyDataReader(List<T> reacords, SqlConnection connection, string schema, string tableName)
         {
-            return false;
+            Records = reacords;
+            _propertyInfos = typeof(T).GetProperties();
+            _nameDictionary = new Dictionary<int, string>();
+            _connection = connection;
+            _schema = schema;
+            _tableName = tableName;
+
+            DataTable schemaTable = GetSchemaTable();
+            for (int x = 0; x < schemaTable?.Rows.Count; x++)
+            {
+                DataRow col = schemaTable.Rows[x];
+                var columnName = col.Field<String>("ColumnName");
+                _nameDictionary.Add(x, columnName);
+            }
         }
-        _currentIndex++;
-        return true;
+
+        public DataTable GetSchemaTable()
+        {
+            using var schemaCommand = new SqlCommand($"SELECT * FROM {_schema}.{_tableName}", _connection);
+            using var reader = schemaCommand.ExecuteReader(CommandBehavior.SchemaOnly);
+            return reader.GetSchemaTable();
+        }
+
+        public bool Read()
+        {
+            if (_currentIndex + 1 >= Records.Count)
+            {
+                return false;
+            }
+            _currentIndex++;
+            return true;
+        }
+
+        public object GetValue(int i)
+        {
+            Car currentRecord = Records[_currentIndex] as Car;
+            return i switch
+            {
+                0 => currentRecord.Id,
+                1 => currentRecord.MakeId,
+                2 => currentRecord.Color,
+                3 => currentRecord.PetName,
+                4 => currentRecord.TimeStamp,
+                _ => string.Empty
+            };
+        }
+
+
+        public string GetName(int i) => throw new NotImplementedException();
+        public int GetOrdinal(string name) => throw new NotImplementedException();
+        public string GetDataTypeName(int i) => throw new NotImplementedException();
+        public Type GetFieldType(int i) => throw new NotImplementedException();
+        public int GetValues(object[] values) => throw new NotImplementedException();
+        public bool GetBoolean(int i) => throw new NotImplementedException();
+        public byte GetByte(int i) => throw new NotImplementedException();
+        public long GetBytes(int i, long fieldOffset, byte[] buffer, int bufferoffset, int length) => throw new NotImplementedException();
+        public char GetChar(int i) => throw new NotImplementedException();
+        public long GetChars(int i, long fieldoffset, char[] buffer, int bufferoffset, int length) => throw new NotImplementedException();
+        public Guid GetGuid(int i) => throw new NotImplementedException();
+        public short Getlntl6(int i) => throw new NotImplementedException();
+        public int Getlnt32(int i) => throw new NotImplementedException();
+        public long Getlnt64(int i) => throw new NotImplementedException();
+        public float GetFloat(int i) => throw new NotImplementedException();
+        public double GetDouble(int i) => throw new NotImplementedException();
+        public string GetString(int i) => throw new NotImplementedException();
+        public decimal GetDecimal(int i) => throw new NotImplementedException();
+        public DateTime GetDateTime(int i) => throw new NotImplementedException();
+        public IDataReader GetData(int i) => throw new NotImplementedException();
+        public bool IsDBNull(int i) => throw new NotImplementedException();
+        object IDataRecord.this[int i] => throw new NotImplementedException();
+        object IDataRecord.this[string name] => throw new NotImplementedException();
+        public void Close() => throw new NotImplementedException();
+        public bool NextResultO => throw new NotImplementedException();
+        public int Depth { get; }
+        public bool IsClosed { get; }
+        public int RecordsAffected { get; }
+
     }
 }

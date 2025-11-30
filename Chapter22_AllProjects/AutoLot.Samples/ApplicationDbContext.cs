43dab47c46dd59fbd52be805873a1f62ebc12c60
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-11-30 13:45:30 +0300

    start work

--- Chapter22_AllProjects/AutoLot.Samples/ApplicationDbContext.cs
+++ Chapter22_AllProjects/AutoLot.Samples/ApplicationDbContext.cs
@@ -23,17 +23,36 @@ namespace AutoLot.Samples
                 entity.HasIndex(e => e.MakeId, "IX_Inventory_MakeId");
                 entity.Property(e => e.Color)
                 .IsRequired()
-                .HasMaxLength(50);
+                .HasMaxLength(50)
+                .HasDefaultValue("Black");
                 entity.Property(e => e.PetName)
                 .IsRequired()
                 .HasMaxLength(50);
+                entity.Property(e => e.FullName)
+                .HasComputedColumnSql("[PetName] + '('+ [Color] + ')'", stored : true);
                 entity.Property(e => e.TimeStamp)
                 .IsRowVersion()
                 .IsConcurrencyToken();
-
+                entity.Property(e => e.isDrivale)
+                .HasField("_isDrivable")
+                .HasDefaultValue(true);
+                entity.HasOne(d => d.MakeNavigations)
+                .WithMany(p => p.Cars)
+                .HasForeignKey(d => d.MakeId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Inventory_Makes_MakeId");
             });
-        }
 
+            modelBuilder.Entity<Radio>(entity => 
+            {
+                entity.HasIndex(e => e.CarId, "IX_Radios_CarId")
+                .IsUnique();
+                entity.HasOne(d => d.CarNavigation)
+                .WithOne(p => p.RadioNavigation)
+                .HasForeignKey<Radio>(d => d.CarId);
+            }); 
+        }
+         
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
--- Chapter22_AllProjects/AutoLot.Samples/Models/Car.cs
+++ Chapter22_AllProjects/AutoLot.Samples/Models/Car.cs
@@ -8,9 +8,17 @@ namespace AutoLot.Samples.Models
     {
         public string Color { get; set; }
         public string PetName { get; set; }
+        public string FullName { get; set; }
         public int MakeId { get; set; }
         public Make MakeNavigations { get; set; }
         public Radio RadioNavigation { get; set; }
         public IEnumerable<Driver> Drivers { get; set; } = new List<Driver>();
+
+        private bool? _isDrivable;
+        public bool isDrivale 
+        {
+            get => _isDrivable ?? true;
+            set => _isDrivable = value;
+        }
     }
 }

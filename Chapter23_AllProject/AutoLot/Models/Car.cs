dcc28d906e15e10431b5163d42dded0fb107c37d
Borissov M.R. <borissov.mikhail2374@mail.ru>
2025-11-30 14:34:59 +0300

    add models

---
+++ Chapter23_AllProject/AutoLot/Models/Car.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AutoLot.Samples.Models
+{
+    public class Car : BaseEntity
+    {
+        public string Color { get; set; }
+        public string PetName { get; set; }
+        public string FullName { get; set; }
+        public int MakeId { get; set; }
+        public Make MakeNavigations { get; set; }
+        public Radio RadioNavigation { get; set; }
+        public IEnumerable<Driver> Drivers { get; set; } = new List<Driver>();
+
+        private bool? _isDrivable;
+        public bool isDrivale 
+        {
+            get => _isDrivable ?? true;
+            set => _isDrivable = value;
+        }
+    }
+}
---
+++ Chapter23_AllProject/AutoLot/Models/CarDrvier.cs
@@ -0,0 +1,8 @@
+namespace AutoLot.Samples.Models
+{
+    public class CarDriver
+    {
+        public int CarId { get; set; }
+        public int DriverId { get; set; }
+    }
+}

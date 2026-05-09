3e95754552d2b787e57b0632bceca7f9fa6a7f07
Borissov M.R. <borissov.mikhail2374@mail.ru>
2026-05-09 14:54:12 +0300

    be back on java

---
+++ java/bootConfigDemo/src/main/java/com/example/bootConfigDemo/BootConfigDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootConfigDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootConfigDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootConfigDemoApplication.class, args);
+	}
+
+}
---
+++ java/bootConfigDemo/src/test/java/com/example/bootConfigDemo/BootConfigDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootConfigDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootConfigDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

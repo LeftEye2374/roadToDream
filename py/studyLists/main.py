88a648f038de225a14ff2894ddb8b9b3221788e7
Borissov M.R. <borissov.mikhail2374@mail.ru>
2026-06-12 13:54:24 +0300

    Operation with lists

---
+++ py/studyLists/main.py
@@ -0,0 +1,10 @@
+nums = [1,2,10,15,25,32,5]
+print("Список чисел: ",nums)
+print("Длина списка: ", len(nums))
+print(nums[0])
+print(nums[-1])
+print(min(nums))
+print(max(nums))
+print(sorted(nums))
+print(sorted(nums, reverse=True))
+
